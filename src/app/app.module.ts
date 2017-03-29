941ba4ce98a9402370b2f3413da958170597c0b6
Eric.Zhao <Eric.Zhao@dominos.com.au>
2017-03-29 10:12:33 +1000

    Cheating test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BestScoreManager } from './app.storage.service'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
+    BrowserModule
   ],
-  providers: [],
+  providers: [ 
+    BestScoreManager
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
